78358ef40766528151699110bb1f51c0f7a547e8
Andrey <andreyoala@icloud.com>
2022-08-31 12:05:07 +0300

    Initial Commit

---
+++ HW17-AndreyOala/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW17-AndreyOala
+//
+//  Created by Andrey Oala on 31.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW17-AndreyOala/HW17_AndreyOalaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW17_AndreyOalaApp.swift
+//  HW17-AndreyOala
+//
+//  Created by Andrey Oala on 31.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct HW17_AndreyOalaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
